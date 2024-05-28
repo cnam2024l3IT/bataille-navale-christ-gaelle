4e11183101f963b1d7edeec372c39e0c31cd1d33
christopher TUHITI <christopher@ios.pf>
2024-05-27 21:14:38 -1000

    update class Grille

--- BatailleNavaleChristGaelle/src/pf/cnam/npf121/bataillenavale/models/Grille.java
+++ BatailleNavaleChristGaelle/src/pf/cnam/npf121/bataillenavale/models/Grille.java
@@ -3,9 +3,10 @@ package pf.cnam.npf121.bataillenavale.models;
 import java.util.ArrayList;
 
 public abstract class Grille {
-	protected ArrayList<CustomCase> customCases;
+	protected ArrayList<CustomCase> customCases = new ArrayList<>();
 	
 	public Grille () {
+		// appel methode qui initialise les 100 cases
 		
 	}
 
